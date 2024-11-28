2429c12932c73b46e6900a8502e6c490ca483ed2
Olga16119 <tkachenko93@ukr.net>
2024-11-28 22:38:01 +0100

    added variables and functions

--- study.py
+++ study.py
@@ -1,1 +1,44 @@
 print("Hello World")
+
+
+# number 1
+name = 'Kate'
+age = 23
+flt = 1,15
+isMarried = false
+mounths = ['May','June','July']
+dictionary = {'name':'Piter', 'age':45, 'smoke':false}
+tpl = (15, 16,17,18)
+none = None
+
+# number 2
+highNumber = age > flt and dictionary[age] not in tpl 
+result = name != none 
+dictionary[name]== name
+
+# number 3
+num_str = 125
+num_string=str(num_str)
+
+message="Hi, my name is Python!"
+changeMessage = message.replace('y',"o" and "i", "I")
+
+split_test= "This is a split text"
+splitText=split_test.split()
+string_join= splitText.join(' ')
+string_join.len()
+
+list_append=[1,2,3]
+list_append.append(4,5)
+
+list_extend = [4,5,6]
+list_extend.extend{[7,8,9]}
+list_extend.index(6)
+list_append.len()
+
+dict_test= {'car':'Toyota', "price":4900, "where":"EU"}
+dict_test[car]
+dict_test[where]
+dict_test.keys()
+dict_test.values()
+dict_test.items()
